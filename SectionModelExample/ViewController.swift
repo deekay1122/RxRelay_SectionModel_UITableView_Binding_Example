3dabf17b299e9b25417bfcc68edfc94f14de3af0
deekay1122 <deekay1122@gmail.com>
2022-11-18 17:55:37 +0900

    Initial Commit

---
+++ SectionModelExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SectionModelExample
+//
+//  Created by Daisaku Ejiri on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
